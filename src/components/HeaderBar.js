172da7539d0d2d34287340c670565823fce1766f
Wang Biqing <wang_bi@worksap.co.jp>
2018-11-30 15:06:42 +0800

    refs#4 remove search bar

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -1,74 +1,24 @@
 import AppBar from "@material-ui/core/AppBar/AppBar";
 import Button from "@material-ui/core/es/Button/Button";
-import InputBase from "@material-ui/core/InputBase/InputBase";
 import {withStyles} from '@material-ui/core/styles';
-import {fade} from '@material-ui/core/styles/colorManipulator';
 import Toolbar from "@material-ui/core/Toolbar/Toolbar";
-import Search from '@material-ui/icons/Search';
+import Typography from "@material-ui/core/Typography/Typography";
 import React, {Component} from "react";
 import Link from "react-router-dom/es/Link";
 import logo from '../pictures/teamSpiritLogo.PNG';
 
-const styles = theme => ({
+const styles = {
   toolBar: {
     alignItems: 'center',
     justifyContent: 'space-between',
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing.unit * 2,
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing.unit * 3,
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    width: theme.spacing.unit * 9,
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-    width: '100%',
-  },
-  inputInput: {
-    paddingTop: theme.spacing.unit,
-    paddingRight: theme.spacing.unit,
-    paddingBottom: theme.spacing.unit,
-    paddingLeft: theme.spacing.unit * 10,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: 200,
-    },
   },
   image: {
     width: 30,
     height: 30,
   }
-});
+};
 
 class HeaderBar extends Component {
-  constructor(props) {
-    super(props);
-    this.handleKeyUp = this.handleKeyUp.bind(this);
-  }
-
-  handleKeyUp(event) {
-    if (event.keyCode === 13) {
-    }
-  };
 
   render() {
     const {classes} = this.props;
@@ -76,18 +26,10 @@ class HeaderBar extends Component {
       <AppBar position="fixed">
         <Toolbar className={classes.toolBar}>
           <img className={classes.image} src={logo} alt="TeamSpirit"/>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <Search onClick={this.handleClick}/>
-            </div>
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              onKeyDown={this.handleKeyUp}
-            />
+          <div>
+            <Typography align="center" variant="h5" color="inherit">
+              Shuffle Player
+            </Typography>
           </div>
           {
             this.props.isLoggedIn ?
